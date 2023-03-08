f0a827b4edacae2f6891f34c74fb85298fee68c2
Merrick Liu <merrickliu168@gmail.com>
2023-03-08 01:40:36 -0500

    Update WorldGraph.py

--- WorldGraph.py
+++ WorldGraph.py
@@ -1,16 +1,23 @@
+class Country: # Equivalent to Vertex
+    name: str
+
+    def __init__(self, name: str):
+        self.name = name
+
+class ExportingCountry(Country): # Exporting Countries
+    # Vaccine Export Rate
+    # Vaccine Stored Amount
+    pass
+
+class Edge: # Equivalent to Edge
+    exporter: ExportingCountry
+    importer: Country
+    shipment_time: float
+
 class World: # Equivalent to Graph
-    countries: set
-    edges: set
+    countries: set[Country | ExportingCountry]
+    edges: set[Edge]
 
     def __init__(self, countries: set, edges: set):
         self.countries = countries
         self.edges = edges
-
-class Country: # Equivalent to Vertex
-    pass
-
-class Edge: # Equivalent to Edge
-    pass
-
-class ExportingCountry(Country): # Exporting Countries
-    pass
