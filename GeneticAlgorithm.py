10e9bfb0f8a2925981dfd7a21d77e142f72ca32c
Ganesh Asapu <62232361+ganeshasapu@users.noreply.github.com>
2023-03-08 01:17:59 -0500

    added initial files

---
+++ GeneticAlgorithm.py
@@ -0,0 +1,27 @@
+class GeneticAlgorithm:
+    mutation_rate: float
+    crossover_rate: float
+    replication_rate: float
+
+    chromosone_size: int
+    num_chromosones: int
+
+
+    def __init__(self, mutation_rate: float, crossover_rate: float, replication_rate: float, chromosone_size: int, num_chromosones: int):
+        self.mutation_rate = mutation_rate
+        self.crossover_rate = crossover_rate
+        self.replication_rate = replication_rate
+        self.chromosone_size = chromosone_size
+        self.num_chromosones = num_chromosones
+    
+
+
+    def run(self):
+        pass
+
+    def initial_chromosone(self):
+        pass
+
+    def fitness(self):
+        pass
+
